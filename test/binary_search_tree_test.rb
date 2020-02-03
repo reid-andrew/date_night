5baf96d8b70f76d13394911492dd8f173d6da253
Andrew Reid <areid.sj@gmail.com>
2020-02-03 10:09:12 -0700

    Write max test

--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -45,4 +45,8 @@ class BinarySearchTreeTest < Minitest::Test
     assert_equal 1, @tree.depth_of(92)
     assert_equal 2, @tree.depth_of(50)
   end
+
+  def test_it_finds_max
+    assert_equal ({"Sharknado 3"=>92}), @tree.max
+  end
 end
